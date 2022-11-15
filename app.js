a24ed75d933a9c27e2c1eb6605afe7a6af624ce6
hamza <hamza.m2113@gmail.com>
2022-11-15 14:43:38 +0000

    tests and methods completed for route /api/reviews

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getCategories } = require("./controllers/categories.controllers");
+const {
+  getCategories,
+  getReviews,
+} = require("./controllers/games.controllers");
 const { catchAll } = require("./controllers/errors.controllers");
 const app = express();
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews", getReviews);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Not found!" });
